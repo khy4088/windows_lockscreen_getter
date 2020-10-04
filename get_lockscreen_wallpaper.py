b7bccd018d0a7493a5c20c14a9be2e38f1ade935
PrestoLive <khy4088@gmail.com>
2020-10-04 20:50:34 +0900

    PIL 사용, 파일크기로 판별에서 이미지 크기로 판별로 수정

--- get_lockscreen_wallpaper.py
+++ get_lockscreen_wallpaper.py
@@ -1,8 +1,8 @@
 import os
 from os import rename, listdir
 import shutil
-import sys
 from datetime import datetime
+from PIL import Image
 
 origin = os.getenv('LOCALAPPDATA') + '\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets'
 export_location = datetime.today().strftime("%Y%m%d%H%M%S")[2:]
@@ -10,9 +10,13 @@ export_location = datetime.today().strftime("%Y%m%d%H%M%S")[2:]
 shutil.copytree(origin,  export_location)
 
 for filename in os.listdir(export_location):
-    print(filename)
-    if os.path.getsize(export_location + '/' + filename) / 1024 < 200:
+    i = Image.open(export_location + '/' + filename)
+    width, height = i.size
+    print(width, height)
+    i.close()
+    if width < 700 or height < 700:
         os.remove(export_location + '/' + filename)
-
-for remain in os.listdir(export_location):
-    os.rename(export_location + '/' + remain, export_location + '/' + remain + '.jpg')
+    else:
+        os.rename(export_location + '/' + filename, filename + '.jpg')
+        
+os.rmdir(export_location)
